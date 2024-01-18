579eb8c85b265f82b19d22b8197834f1c806635c
Olivia Gee <108471746+geeolivia@users.noreply.github.com>
2024-01-18 11:43:26 -0500

    created basic page

---
+++ src/About.js
@@ -0,0 +1,37 @@
+import './About.css'
+
+const About = () => {
+  const about = {
+    name: 'Olivia Gee',
+    role: 'Computer Science Graduate',
+    description: 'Front-End, Back-End Developer',
+    resume: 'Pretend This link works',
+  };
+
+  const { name, role, description, resume } = about
+
+  return (
+    <div className='about center'>
+      {name && (
+        <h1>
+          Hi, I am <span className='about__name'>{name}.</span>
+        </h1>
+      )}
+
+      {role && <h2 className='about__role'>A {role}.</h2>}
+      <p className='about__desc'>{description && description}</p>
+
+      <div className='about__contact center'>
+        {resume && (
+          <a href={resume}>
+            <span type='button' className='btn btn--outline'>
+              Resume
+            </span>
+          </a>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default About
