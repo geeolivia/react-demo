0727342c832b8a297384c058152317100b317e70
Olivia Gee <108471746+geeolivia@users.noreply.github.com>
2024-01-22 00:01:50 -0500

    Added game and styles

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import './App.css';
 //import Header from './Header'
 import About from './About'
 //import Footer from './Footer'
+import Game from './Game.js'
 
-
+//main component named 'App'
 const App = () => {
-
   return (
-    <div>
-      <main>
+    /* main div container */
+    <div className="container">
+      <div>
         <About />
-      </main>
-      
+      </div>
+      <div>
+        <Game />
+      </div>
     </div>
-  )
-}
+  );
+};
 
+//export 'App' component
 export default App
+
+
